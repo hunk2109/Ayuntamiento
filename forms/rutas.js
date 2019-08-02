32f931acb5b0d1e38e490914d5c7deefd7f1da8f
Hector <hectojo_2109@hotmail.com>
2019-08-02 14:09:37 -0400

    3

--- forms/rutas.js
+++ forms/rutas.js
@@ -1,5 +1,5 @@
 import React ,{ Component } from 'react';
-import { StyleSheet, Text, View,KeyboardAvoidingView,FlatList,StatusBar } from 'react-native';
+import { StyleSheet, Text, View,KeyboardAvoidingView,FlatList,StatusBar,TouchableOpacity } from 'react-native';
 import {Input,Card,Button,Icon, Item} from 'native-base'
 import * as firebase from 'firebase'
 import MapView from 'react-native-maps'
@@ -11,6 +11,10 @@ import { SearchBar } from 'react-native-elements';
 
 
 export default class  rutas extends React.Component { 
+
+  rutasb=()=>{
+    
+  }
 
   constructor(props) {
     super(props);
@@ -26,6 +30,14 @@ export default class  rutas extends React.Component {
  
 
     render(){  
+
+      let data = [{
+        value: '1',
+      }, {
+        value: 'Mango',
+      }, {
+        value: 'Pear',
+      }];
       
       var mapStyle=[{"elementType": "geometry", "stylers": [{"color": "#242f3e"}]},{"elementType": "labels.text.fill","stylers": [{"color": "#746855"}]},{"elementType": "labels.text.stroke","stylers": [{"color": "#242f3e"}]},{"featureType": "administrative.locality","elementType": "labels.text.fill","stylers": [{"color": "#d59563"}]},{"featureType": "poi","elementType": "labels.text.fill","stylers": [{"color": "#d59563"}]},{"featureType": "poi.park","elementType": "geometry","stylers": [{"color": "#263c3f"}]},{"featureType": "poi.park","elementType": "labels.text.fill","stylers": [{"color": "#6b9a76"}]},{"featureType": "road","elementType": "geometry","stylers": [{"color": "#38414e"}]},{"featureType": "road","elementType": "geometry.stroke","stylers": [{"color": "#212a37"}]},{"featureType": "road","elementType": "labels.text.fill","stylers": [{"color": "#9ca5b3"}]},{"featureType": "road.highway","elementType": "geometry","stylers": [{"color": "#746855"}]},{"featureType": "road.highway","elementType": "geometry.stroke","stylers": [{"color": "#1f2835"}]},{"featureType": "road.highway","elementType": "labels.text.fill","stylers": [{"color": "#f3d19c"}]},{"featureType": "transit","elementType": "geometry","stylers": [{"color": "#2f3948"}]},{"featureType": "transit.station","elementType": "labels.text.fill","stylers": [{"color": "#d59563"}]},{"featureType": "water","elementType": "geometry","stylers": [{"color": "#17263c"}]},{"featureType": "water","elementType": "labels.text.fill","stylers": [{"color": "#515c6d"}]},{"featureType": "water","elementType": "labels.text.stroke","stylers": [{"color": "#17263c"}]}];
 
@@ -35,11 +47,20 @@ export default class  rutas extends React.Component {
           
 <View style={styles.container}>
 
-  
+if (data == "1") {
   <MapView
-    initialRegion={this.state.region} style={styles.map}
-    customMapStyle={mapStyle}
-  />
+  initialRegion={this.state.region} style={styles.map}
+  customMapStyle={mapStyle} 
+/>
+}
+  
+  
+  <View style={{width: 200, height: 44}}>
+  <Dropdown style={styles.overlay}
+        label='Sector'
+        data={data}
+      />
+    </View>
   
     
   
